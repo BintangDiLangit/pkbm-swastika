46ee3ccb5db76bb3fa5044076a2d95730dd6e34d
bintangmfhd <bintangmfhd@gmail.com>
2024-08-30 22:57:08 +0700

    update seo

--- src/resources/views/layouts/head.blade.php
+++ src/resources/views/layouts/head.blade.php
@@ -1,8 +1,16 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>@yield('title')</title>
-    <meta name="description" content="">
+    <title>PKBM Swastika - Pendidikan Berkualitas di Indonesia</title>
+    <meta name="description"
+        content="PKBM Swastika menawarkan pendidikan berkualitas dengan program belajar yang fleksibel di Indonesia. Daftar sekarang!">
+    <meta name="keywords" content="PKBM, Swastika, Pendidikan, Indonesia, Program Belajar, Pendidikan Berkualitas">
+
+    <meta property="og:title" content="PKBM Swastika - Pendidikan Berkualitas di Indonesia" />
+    <meta property="og:description"
+        content="PKBM Swastika menawarkan pendidikan berkualitas dengan program belajar yang fleksibel di Indonesia. Daftar sekarang!" />
+    <meta property="og:image" content="{{ asset('assets/img//og-image.jpg') }}" />
+    <meta property="og:url" content="https://pkbmswastika.sch.id/" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.ico') }}">
