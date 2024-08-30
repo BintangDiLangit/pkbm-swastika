8347c86880ec9d16de389e3d4ca5657005fa94fd
bintangmfhd <bintangmfhd@gmail.com>
2024-08-30 21:40:29 +0700

    only light mode

--- src/resources/views/layouts/script.blade.php
+++ src/resources/views/layouts/script.blade.php
@@ -18,12 +18,5 @@
  <script src="{{ asset('assets/js/main.js') }}"></script>
 
  <script>
-     // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-     if (localStorage.getItem("theme-color") === "dark" || (!("theme-color" in localStorage) && window.matchMedia(
-             "(prefers-color-scheme: dark)").matches)) {
-         document.getElementById("light--to-dark-button")?.classList.add("dark--mode");
-     }
-     if (localStorage.getItem("theme-color") === "light") {
-         document.getElementById("light--to-dark-button")?.classList.remove("dark--mode");
-     }
+     document.getElementById("light--to-dark-button")?.classList.remove("dark--mode");
  </script>
